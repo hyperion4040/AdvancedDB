4ba00eb37ec796530fb4fcaf0479504b5ad08d3a
hyperion4040 <hyperion1987@wp.pl>
2017-02-22 16:02:52 +0100

    my second commit

--- wsb.sql
+++ wsb.sql
@@ -2,6 +2,8 @@ set serveroutput on
 
 declare 
  message VARCHAR2(20) := 'hello oracle';
+ name VARCHAR2(10) := 'Adrian';
  begin
  DBMS_OUTPUT.PUT_LINE(message);
+ DBMS_OUTPUT.put_line(name);
  END; 
