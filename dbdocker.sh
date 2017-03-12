330b3bbf0c9b9fafc227b87792535c747a171a22
hyperion4040 <hyperion1987@wp.pl>
2017-03-12 10:07:10 +0100

    Add connection to Apex on port 49162

--- dbdocker.sh
+++ dbdocker.sh
@@ -5,6 +5,6 @@ if [ "$(sudo docker ps -aq -f status=exited -f status=created)" ]; then
 echo "Container of Oracle DB with id = $(sudo docker rm $(sudo docker ps -q -f status=exited -f status=created)) was removed"
 fi
 else
-echo "Container of Oracle DB with id = $(sudo docker run -d -p 49160:22 -p 49161:1521 wnameless/oracle-xe-11g) had been created and is running"
+echo "Container of Oracle DB with id = $(sudo docker run -d -p 49160:22 -p 49161:1521 -p 49162:8080 wnameless/oracle-xe-11g) had been created and is running"
 fi
 
