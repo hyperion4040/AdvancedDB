c44385622498fce2cd41792d653b5b56f27ee3f7
hyperion4040 <hyperion1987@wp.pl>
2017-04-08 12:25:54 +0200

    refactor of third line

--- wsb.sql
+++ wsb.sql
@@ -1,10 +1,10 @@
-set serveroutput on
-
+/*set serveroutput on
+*/
 declare 
  message VARCHAR2(20) := 'hello oracle';
  name VARCHAR2(10) := 'Adrian';
  begin
  DBMS_OUTPUT.PUT_LINE(message);
  DBMS_OUTPUT.put_line(name);
- DBMS_OUTPUT.PUT_LINE('My name is ' || name);
+ DBMS_OUTPUT.PUT_LINE('My name is ' || name || ' and my message is ' || message);
  END; 
